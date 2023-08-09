fcc748a362f4db810a4bcc0489abee15d5f8cd45
Brice McIver <github@bricemciver.com>
2023-08-09 13:11:10 -0500

    fix: update to support current SMPost website

--- dist/shawnee-mission-post-paywall-remover/shawnee-mission-post-paywall-remover.user.js
+++ dist/shawnee-mission-post-paywall-remover/shawnee-mission-post-paywall-remover.user.js
@@ -12,5 +12,37 @@
 // @grant        none
 // ==/UserScript==
 {
-    Array.from(document.getElementsByClassName('not-logged-in')).forEach(function (element) { return element.classList.remove('not-logged-in'); });
+    // Callback function to handle mutations
+    var mutationCallback_1 = function (mutationsList, _observer) {
+        for (var _i = 0, mutationsList_1 = mutationsList; _i < mutationsList_1.length; _i++) {
+            var mutation = mutationsList_1[_i];
+            if (mutation.type === 'childList') {
+                mutation.addedNodes.forEach(function (addedNode) {
+                    if (addedNode.nodeType === Node.ELEMENT_NODE && addedNode.id === 'content-paywall-block') {
+                        addedNode.remove();
+                    }
+                });
+            }
+            if (mutation.type === 'attributes' &&
+                mutation.target.nodeType === Node.ELEMENT_NODE &&
+                mutation.target.classList.contains('not-logged-in')) {
+                mutation.target.classList.remove('not-logged-in');
+            }
+        }
+    };
+    var initObserver = function () {
+        // Create a new observer instance with the callback function
+        var observer = new MutationObserver(mutationCallback_1);
+        // Target a specific node
+        var entry = document.querySelector('div.entry-content');
+        if (entry) {
+            // Start observing the target node for specified mutations
+            observer.observe(entry, {
+                childList: true,
+                subtree: true,
+                attributeFilter: ['class'],
+            });
+        }
+    };
+    initObserver();
 }
--- src/main/shawnee-mission-post-paywall-remover/shawnee-mission-post-paywall-remover.user.ts
+++ src/main/shawnee-mission-post-paywall-remover/shawnee-mission-post-paywall-remover.user.ts
@@ -11,5 +11,42 @@
 // @grant        none
 // ==/UserScript==
 {
-  Array.from(document.getElementsByClassName('not-logged-in')).forEach(element => element.classList.remove('not-logged-in'));
+  // Callback function to handle mutations
+  const mutationCallback: MutationCallback = (mutationsList, _observer) => {
+    for (const mutation of mutationsList) {
+      if (mutation.type === 'childList') {
+        mutation.addedNodes.forEach(addedNode => {
+          if (addedNode.nodeType === Node.ELEMENT_NODE && (addedNode as Element).id === 'content-paywall-block') {
+            (addedNode as Element).remove();
+          }
+        });
+      }
+      if (
+        mutation.type === 'attributes' &&
+        mutation.target.nodeType === Node.ELEMENT_NODE &&
+        (mutation.target as Element).classList.contains('not-logged-in')
+      ) {
+        (mutation.target as Element).classList.remove('not-logged-in');
+      }
+    }
+  };
+
+  const initObserver = () => {
+    // Create a new observer instance with the callback function
+    const observer = new MutationObserver(mutationCallback);
+
+    // Target a specific node
+    const entry = document.querySelector<HTMLDivElement>('div.entry-content');
+
+    if (entry) {
+      // Start observing the target node for specified mutations
+      observer.observe(entry, {
+        childList: true, // Watch for changes in the child nodes
+        subtree: true, // Watch all nested nodes as well
+        attributeFilter: ['class'],
+      });
+    }
+  };
+
+  initObserver();
 }
