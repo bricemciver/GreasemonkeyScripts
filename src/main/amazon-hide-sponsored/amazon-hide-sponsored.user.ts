f32111bf6f77c9ec558a9c26a82efbb9665c8fd6
Brice McIver <github@bricemciver.com>
2024-06-05 11:29:42 -0500

    feat: New script to hide Amazon sponsored results

---
+++ src/main/amazon-hide-sponsored/amazon-hide-sponsored.user.ts
@@ -0,0 +1,21 @@
+(() => {
+  const findAndRemoveSponsoredItems = () => {
+    const sponsoredItems = document.evaluate("//span[text()='Sponsored']", document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+    for (let i = 0; i < sponsoredItems.snapshotLength; i++) {
+      const node = sponsoredItems.snapshotItem(i);
+      if (node && node.nodeType === Node.ELEMENT_NODE) {
+        // get the parent with a non-empty data-asin attribute
+        let parent: HTMLElement = node as HTMLElement;
+        while (parent && (!parent.hasAttribute('data-asin') || parent.getAttribute('data-asin') === '') && parent.parentElement) {
+          parent = parent.parentElement;
+        }
+        // hide the parent if it has a non-empty data-asin attribute
+        if (parent && parent.hasAttribute('data-asin') && parent.getAttribute('data-asin') !== '') {
+          parent.style.display = 'none';
+        }
+      }
+    }
+  };
+  findAndRemoveSponsoredItems();
+})();
